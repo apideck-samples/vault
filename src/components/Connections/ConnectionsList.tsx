4fd68eb51766156364a0730f8a32e6126c3ffd29
Jake Prins <jakeprins@outlook.com>
2021-06-30 09:55:18 +0200

    Improve search (#63)

--- src/components/Connections/ConnectionsList.tsx
+++ src/components/Connections/ConnectionsList.tsx
@@ -4,9 +4,10 @@ import { useContext, useState } from 'react'
 
 import { Button } from '@apideck/components'
 import { IConnection } from 'types/Connection'
+import Link from 'next/link'
 
 interface IProps {
-  unifiedApi: string
+  unifiedApi?: string
   connections: IConnection[]
   createConnection: (
     values: { unifiedApi: string; serviceId: string },
@@ -24,32 +25,46 @@ const ConnectionsList = ({ unifiedApi, connections = [], createConnection }: IPr
 
   return (
     <section className="mt-8 lg:mt-12" data-testid={'connections-list'}>
-      <div className="flex items-center justify-between mb-4 sm:mb-6 md:mb-8">
-        <h2 className="text-sm font-medium uppercase">{unifiedApi} integrations</h2>
-        <Button
-          text="+ Add"
-          onClick={() => setModalOpen(true)}
-          className="spec-add-integration"
-          style={primary_color ? { backgroundColor: primary_color } : {}}
-        />
-      </div>
+      {unifiedApi ? (
+        <div className="flex items-center justify-between mb-4 sm:mb-6 md:mb-8">
+          <h2 className="text-sm font-medium uppercase">{unifiedApi} integrations</h2>
+          <Button
+            text="+ Add"
+            onClick={() => setModalOpen(true)}
+            className="spec-add-integration"
+            style={primary_color ? { backgroundColor: primary_color } : {}}
+          />
+        </div>
+      ) : (
+        ''
+      )}
       {addedConnections.map((connection) => {
-        const { id } = connection
+        const { id, unified_api, service_id } = connection
 
-        return <ConnectionCard key={id} connection={connection} />
+        return (
+          <Link href={`/integrations/${unified_api}/${service_id}`} key={id}>
+            <a>
+              <ConnectionCard connection={connection} />
+            </a>
+          </Link>
+        )
       })}
       {noConnectionsAdded && (
         <div className="mt-5 text-sm italic text-gray-500">
           No {unifiedApi} integrations added yet.
         </div>
       )}
-      <AddModal
-        open={modalOpen}
-        setOpen={setModalOpen}
-        unifiedApi={unifiedApi}
-        availableConnections={availableConnections}
-        createConnection={createConnection}
-      />
+      {unifiedApi ? (
+        <AddModal
+          open={modalOpen}
+          setOpen={setModalOpen}
+          unifiedApi={unifiedApi}
+          availableConnections={availableConnections}
+          createConnection={createConnection}
+        />
+      ) : (
+        ''
+      )}
     </section>
   )
 }
