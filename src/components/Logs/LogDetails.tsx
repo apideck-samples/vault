f34abbcd272af30946ba50bbad16948d600d47f5
Jake Prins <jakeprins@outlook.com>
2021-07-15 11:39:33 +0200

    Adjust spacing in log details

--- src/components/Logs/LogDetails.tsx
+++ src/components/Logs/LogDetails.tsx
@@ -52,7 +52,7 @@ const LogDetails = ({ log }) => {
         <img src={getIcon()} className="block w-8 h-8 text-gray-900 rounded" />
       </div>
       <div className="px-4 py-5 border-t border-gray-200 sm:px-6">
-        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2">
           <div className="sm:col-span-1">
             <dt className="text-sm font-medium text-gray-500">API</dt>
             <dd className="mt-1 text-sm text-gray-900">
