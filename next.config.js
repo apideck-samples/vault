29e4177194fb663f1f8de44cb394a70a1f77db37
Jake Prins <jakeprins@outlook.com>
2021-07-20 15:03:54 +0200

    Update next.config.js to include security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
+const securityHeaders = [
+  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: 'X-UA-Compatible', value: 'IE=Edge' },
+  { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'Referrer-Policy', value: 'no-referrer' },
+  { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubdomains; always' },
+  {
+    key: 'Permissions-Policy',
+    value: 'fullscreen=(self "https://app.apideck.com"), geolocation=*, camera=()'
+  }
+]
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ]
+  },
   experimental: {
     productionBrowserSourceMaps: true
   }
