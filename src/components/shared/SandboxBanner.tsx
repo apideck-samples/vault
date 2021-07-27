b0bb9fddf499711dce797172f4a61e8b63593e43
Gertjan De Wilde <info@gertjandewilde.com>
2021-07-27 14:19:10 +0200

    Update SandboxBanner.tsx

--- src/components/shared/SandboxBanner.tsx
+++ src/components/shared/SandboxBanner.tsx
@@ -1,9 +1,9 @@
 const SandboxBanner = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 z-50">
-      <div className="bg-primary-600">
+      <div className="bg-rose-50">
         <div className="px-2.5 py-2.5 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <p className="font-medium text-center text-white truncate">Sandbox mode</p>
+          <p className="font-medium text-center text-red-800 truncate">You are currently in Sandbox mode.</p>
         </div>
       </div>
     </div>
