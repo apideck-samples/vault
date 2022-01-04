c3dedcdc739972cb848ed8f6fa13d7460a82dd86
Jake Prins <jakeprins@outlook.com>
2022-01-04 16:54:45 +0100

    Remove unused values in useSession hook

--- src/utils/useSession.tsx
+++ src/utils/useSession.tsx
@@ -1,9 +1,6 @@
 import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from 'react'
 
 import { JWTSession } from 'types/JWTSession'
-import { createVaultSession } from './createVaultSession'
-import { useRouter } from 'next/router'
-import { useToast } from '@apideck/components'
 
 interface ContextProps {
   createSession: () => Promise<void>
@@ -16,32 +13,9 @@ const SessionContext = createContext<Partial<ContextProps>>({})
 
 export const SessionProvider = ({ children }: { children: ReactNode }) => {
   const [session, setSession] = useState<JWTSession | null>(null)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { push } = useRouter()
-  const { addToast } = useToast()
-
-  // Creates a test session with using the consumerID in your env file or a random consumerID
-  const createSession = async () => {
-    setIsLoading(true)
-    const response = await createVaultSession(`${window.location.href}`)
-
-    if (response.error) {
-      addToast({ title: response.message, description: response.detail, type: 'error' })
-      return
-    }
-
-    const sessionUrl = response?.data?.session_uri
-    const jwt = sessionUrl.substring(sessionUrl.lastIndexOf('/') + 1)
-    if (jwt) {
-      push(`/session/${jwt}`)
-    }
-    setIsLoading(false)
-  }
 
   return (
-    <SessionContext.Provider value={{ createSession, session, setSession, isLoading }}>
-      {children}
-    </SessionContext.Provider>
+    <SessionContext.Provider value={{ session, setSession }}>{children}</SessionContext.Provider>
   )
 }
 
