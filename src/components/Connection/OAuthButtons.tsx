c28a3990719933f44ff7d85c0bbd97a4c9a86706
Jake Prins <jakeprins@outlook.com>
2022-06-17 14:21:35 +0200

    Add Google authorize button to Google Drive connector

--- src/components/Connection/OAuthButtons.tsx
+++ src/components/Connection/OAuthButtons.tsx
@@ -1,10 +1,10 @@
-import React, { memo, useContext } from 'react'
 import { ThemeContext, authorizationVariablesRequired } from 'utils'
+import { memo, useContext } from 'react'
 
 import { Button } from '@apideck/components'
 import { IConnection } from 'types/Connection'
-import classNames from 'classnames'
 import { Theme } from 'types/JWTSession'
+import classNames from 'classnames'
 
 interface IProps {
   connection: IConnection
@@ -17,6 +17,30 @@ interface IProps {
 const OAuthButtons = ({ connection, isAuthorized, isLoading, revokeUrl, onAuthorize }: IProps) => {
   const requiredAuth = authorizationVariablesRequired(connection)
   const { primaryColor } = useContext(ThemeContext) as Theme
+
+  const renderButton = () => {
+    if (connection.service_id === 'google-drive') {
+      return (
+        <button
+          onClick={onAuthorize}
+          disabled={!!requiredAuth || isLoading}
+          className={classNames('h-[40px]', { 'animate-pulse': isLoading })}
+        >
+          <img src="/img/google-button.png" className="h-full" />
+        </button>
+      )
+    }
+
+    return (
+      <Button
+        text={isAuthorized ? 'Re-authorize' : 'Authorize'}
+        onClick={onAuthorize}
+        disabled={!!requiredAuth?.length || isLoading}
+        isLoading={isLoading}
+        style={primaryColor ? { backgroundColor: primaryColor } : {}}
+      />
+    )
+  }
 
   return (
     <div className="flex items-center justify-between px-3 py-2 bg-gray-100 border-t sm:px-4 md:px-5 rounded-bl-md rounded-br-md">
@@ -33,21 +57,8 @@ const OAuthButtons = ({ connection, isAuthorized, isLoading, revokeUrl, onAuthor
         </div>
       )}
 
-      <div>
-        {!requiredAuth && !isLoading ? (
-          <Button
-            text={isAuthorized ? 'Re-authorize' : 'Authorize'}
-            onClick={onAuthorize}
-            style={primaryColor ? { backgroundColor: primaryColor } : {}}
-          />
-        ) : (
-          <Button
-            text={isAuthorized ? 'Re-authorize' : 'Authorize'}
-            disabled={true}
-            isLoading={isLoading}
-            style={primaryColor ? { backgroundColor: primaryColor } : {}}
-          />
-        )}
+      <div className="flex items-center">
+        {renderButton()}
 
         {isAuthorized && connection.oauth_grant_type === 'authorization_code' && (
           <div className="inline-block ml-2 md:ml-4">
