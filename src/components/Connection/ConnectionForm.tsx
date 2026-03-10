082319e8b112cf0218a0ce4940af85b785d373a1
jonaslagoni <jonas-lt@live.dk>
2026-03-10 09:36:30 +0100

    remove log

--- src/components/Connection/ConnectionForm.tsx
+++ src/components/Connection/ConnectionForm.tsx
@@ -329,7 +329,6 @@ const ConnectionForm = ({ connection, token, jwt }: IProps) => {
           applicationId: token.applicationId,
           consumerId: token.consumerId
         })
-        console.log(authorizeUrl)
         window.location.href = authorizeUrl
       } catch (error) {
         setAuthorizeLoading(false)
