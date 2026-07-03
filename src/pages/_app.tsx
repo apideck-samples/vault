a9a5911f8adb1ea8ee0cab916259f34a75e9651b
Gertjan de Wilde <gertjan@apideck.com>
2026-07-02 23:34:17 -0400

    Revert "Add PostHog + HubSpot analytics tracking (#303)"
    This reverts commit eb163f4caf7882865a362e7f6d76489d6ef221f0.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import 'styles/index.css'
 import { ModalProvider, ToastProvider } from '@apideck/components'
 import React, { Fragment, useState } from 'react'
 
-import Analytics from 'components/analytics/Analytics'
 import { AppProps } from 'next/app'
 import { Layout } from 'components'
 import { SessionExpiredModalContext } from 'utils/context'
@@ -13,22 +12,20 @@ const App = ({ Component, pageProps }: AppProps) => {
   const [sessionExpired, setSessionExpired] = useState<boolean>(false)
 
   return (
-    <Analytics source="sample:vault">
-      <Fragment>
-        <SessionExpiredModalContext.Provider value={{ sessionExpired, setSessionExpired }}>
-          <SessionProvider>
-            <ToastProvider>
-              <ModalProvider>
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </ModalProvider>
-            </ToastProvider>
-          </SessionProvider>
-        </SessionExpiredModalContext.Provider>
-        <div id="modal" />
-      </Fragment>
-    </Analytics>
+    <Fragment>
+      <SessionExpiredModalContext.Provider value={{ sessionExpired, setSessionExpired }}>
+        <SessionProvider>
+          <ToastProvider>
+            <ModalProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </ModalProvider>
+          </ToastProvider>
+        </SessionProvider>
+      </SessionExpiredModalContext.Provider>
+      <div id="modal" />
+    </Fragment>
   )
 }
 
